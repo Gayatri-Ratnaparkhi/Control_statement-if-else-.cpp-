a67079f711f6b5aa4a763f1bac3c0a4d64ffecd8
Gayatri <gayatriratnaparkhi3@gmail.com>
2024-09-06 21:35:46 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,22 @@
+//Gayatri Ratnaparkhi
+//23070123169
+#include <iostream>
+using namespace std;
+int main()
+ {
+    int n;
+    cout<<"enter a no"<<endl;
+    cin>>n;
+    if (n%2==0)
+    cout<<"it is even no"<<endl;
+    else
+    cout<<"it is odd no"<<endl;
+    return 0;
+}
+
+
+/*output
+enter a no
+40
+it is even no
+/*
